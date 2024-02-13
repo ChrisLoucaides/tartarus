da3424d49a1b9aa7c6ebff47cf0cdc96e7737c19
ChrisLoucaides <christosloucaides@gmail.com>
2024-02-13 21:34:04 +0000

    FYP-2 fixing login [wip]

--- src/main/java/com/chrisloucaides/tartarus/controller/AuthController.java
+++ src/main/java/com/chrisloucaides/tartarus/controller/AuthController.java
@@ -5,11 +5,13 @@ import com.chrisloucaides.tartarus.util.JwtTokenUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8081")
 public class AuthController {
 
     @Autowired
@@ -19,7 +21,7 @@ public class AuthController {
     private JwtTokenUtil jwtTokenUtil;
 
 
-    @PostMapping("/login")
+    @PostMapping("/userlogin/")
     public ResponseEntity<String> login(@RequestParam String userEmail, @RequestParam String password) { //TODO FYP-2: Refactor me
         boolean isAuthenticated = userService.authenticateUser(userEmail, password);
 
